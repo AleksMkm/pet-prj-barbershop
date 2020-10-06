f248d2018ce9803c56a3fc0c2239e82ccc78f60e
Aleksandr Mukomelov <mukomelov.aleksandr@gmail.com>
2020-10-06 17:58:04 +0300

    adds burger menu closure solution

--- src/index.js
+++ src/index.js
@@ -16,10 +16,32 @@ bodyEl.insertAdjacentHTML('beforeend', markupRefs.teamMarkup);
 bodyEl.insertAdjacentHTML('beforeend', markupRefs.galleryMarkup);
 bodyEl.insertAdjacentHTML('beforeend', markupRefs.footerMarkup);
 
-// бургер-меню
+// бургер-меню -открытие / закрытие меню
 
-import onBurgerClick from './js/burger-menu.js';
+import { onBurgerClick } from './js/burger-menu.js';
 
 const menuBtnEl = document.querySelector('[data-menu-button]');
 
 menuBtnEl.addEventListener('click', onBurgerClick);
+
+// закрытие бекдропа по клику в бекдроп
+
+import { onBackdropClick } from './js/burger-menu.js';
+
+const mobileMenuEl = document.querySelector('[data-menu]');
+
+mobileMenuEl.addEventListener('click', onBackdropClick);
+
+// закрытие бекдропа по клику в навигационные кнопки
+
+import { onModalNavClick } from './js/burger-menu.js';
+
+const modalNavEl = document.querySelector('[data-backdrop-nav]');
+
+modalNavEl.addEventListener('click', onModalNavClick);
+
+import { onModalOnlineClick } from './js/burger-menu.js';
+
+const modalOnlineEl = document.querySelector('[data-backdrop-online]');
+
+modalOnlineEl.addEventListener('click', onModalOnlineClick);
--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,13 +1,65 @@
-export default function onBurgerClick(e) {
-  const mobileMenuEl = document.querySelector('[data-menu]');
+export function onBurgerClick(e) {
   const bodyEl = document.querySelector('body');
-  const expanded = e.target.getAttribute('aria-expanded') === 'true' || false;
+  const mobileMenuEl = document.querySelector('[data-menu]');
+  const expanded =
+    e.currentTarget.getAttribute('aria-expanded') === 'true' || false;
 
-  e.target.classList.toggle('is-open');
+  e.currentTarget.classList.toggle('is-open');
 
   bodyEl.classList.toggle('body-hidden');
 
-  e.target.setAttribute('aria-expanded', !expanded);
+  e.currentTarget.setAttribute('aria-expanded', !expanded);
 
   mobileMenuEl.classList.toggle('is-open');
+
+  // закрытие по клавише esc
+  onEscModalClose();
+}
+
+// реализация закрытия по клавише esc
+
+function onEscModalClose() {
+  window.addEventListener('keydown', onEscKey);
+}
+
+function onEscKey(e) {
+  if (e.code === 'Escape') {
+    onModalClose();
+  }
+}
+
+function onModalClose() {
+  const bodyEl = document.querySelector('body');
+  const mobileMenuEl = document.querySelector('[data-menu]');
+  const menuBtnEl = document.querySelector('[data-menu-button]');
+
+  mobileMenuEl.classList.remove('is-open');
+  menuBtnEl.classList.remove('is-open');
+  bodyEl.classList.remove('body-hidden');
+
+  window.removeEventListener('keydown', onEscKey);
+}
+
+// закрытие по клику в бекдроп
+
+export function onBackdropClick(e) {
+  if (e.currentTarget === e.target) {
+    onModalClose();
+  }
+}
+
+// закрыте по клику в навигационные кнопки
+
+export function onModalNavClick(e) {
+  if (!e.target.dataset.value === 'backdrop-nav') {
+    return;
+  }
+  onModalClose();
+}
+
+export function onModalOnlineClick(e) {
+  if (!e.target.dataset.value === 'backdrop-online') {
+    return;
+  }
+  onModalClose();
 }
